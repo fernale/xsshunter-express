94d5c40176b55fa47d728cdd8a356c3a55a4d1bc
Alexandre Fernandes <alexandretnparaujo@outlook.com>
2025-07-31 21:02:52 +0200

    updating dependencies reaching Vue 3

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,51 +1,34 @@
-/*
- =========================================================
- * Vue Black Dashboard - v1.1.0
- =========================================================
+// src/main.js
 
- * Product Page: https://www.creative-tim.com/product/black-dashboard
- * Copyright 2018 Creative Tim (http://www.creative-tim.com)
+import { createApp, provide } from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import i18n from './i18n'
 
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
- */
-import Vue from "vue";
-import VueRouter from "vue-router";
 import RouterPrefetch from 'vue-router-prefetch'
-import App from "./App";
-// TIP: change to import router from "./router/starterRouter"; to start with a clean layout
-import router from "./router/index";
+import VueClipboard from 'vue-clipboard3'
+import VueMoment from 'vue-moment-v3'
 
-import BlackDashboard from "./plugins/blackDashboard";
-import i18n from "./i18n"
-//import './registerServiceWorker'
-Vue.use(BlackDashboard);
-Vue.use(VueRouter);
-Vue.use(RouterPrefetch);
-Vue.use(require('vue-moment'));
+// vue-codemirror6 imports
+import { Codemirror } from 'vue-codemirror6'
+import { basicSetup } from 'codemirror'
+import { oneDark } from '@codemirror/theme-one-dark'
+import { yaml } from '@codemirror/lang-yaml'
 
-import VueClipboard from 'vue-clipboard2'
-Vue.use(VueClipboard);
 
-import VueCodemirror from 'vue-codemirror'
+const app = createApp(App)
 
-// import base style
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/monokai.css'
-import 'codemirror/mode/yaml/yaml.js'
+app.use(router)
+app.use(i18n)
 
-Vue.use(VueCodemirror);
+app.use(RouterPrefetch)
 
-import VueToastr2 from 'vue-toastr-2'
-import 'vue-toastr-2/dist/vue-toastr-2.min.css'
-window.toastr = require('toastr')
-Vue.use(VueToastr2)
+app.use(VueClipboard)
 
-/* eslint-disable no-new */
-new Vue({
-  router,
-  i18n,
-  render: h => h(App)
-}).$mount("#app");
+app.use(VueMoment)
+
+app.component('Codemirror', Codemirror)
+
+app.provide('cmExtensions', [basicSetup, oneDark, yaml()])
+
+app.mount('#app')
