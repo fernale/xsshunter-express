ef74ded2fb05ed2fbfe94d45d63254daaa0e1212
Alexandre Fernandes <alexandretnparaujo@gmail.com>
2025-08-01 03:28:28 +0200

    :Revert "updating dependencies reaching Vue 3 -adjusts.4"
    This reverts commit 0bf5bf8927c2281d39880e5314e10daa81f7452d.

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -2,7 +2,7 @@ import routes from './routes';
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory('/admin/'), // or createWebHashHistory() if you want hash-based routing
+  history: createWebHistory('/admin'), // or createWebHashHistory() if you want hash-based routing
   routes,
   linkExactActiveClass: "active",
   scrollBehavior(to) {
